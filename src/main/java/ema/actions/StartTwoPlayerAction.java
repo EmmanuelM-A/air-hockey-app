178e2b2826a1ffd420ba91281e440bb0ccf50578
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2024-08-20 15:26:03 +0100

    Working on the two player settings window before continuing with the the single player setting window.

--- src/main/java/ema/actions/StartTwoPlayerAction.java
+++ src/main/java/ema/actions/StartTwoPlayerAction.java
@@ -3,12 +3,16 @@ package ema.actions;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import ema.ui.game.settings.TwoPlayerSetting;
 import ema.ui.game.twoPlayer.TwoPlayerFrame;
 import ema.ui.home.HomeMenuFrame;
 
 public class StartTwoPlayerAction implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
-        HomeMenuFrame.instance.switchFrame(new TwoPlayerFrame());
+        //HomeMenuFrame.instance.switchFrame(new TwoPlayerFrame());
+
+        TwoPlayerSetting setting = new TwoPlayerSetting(HomeMenuFrame.instance);
+        setting.getDialog().setVisible(true);
     }
 }
--- src/main/java/ema/ui/game/settings/TwoPlayerSetting.java
+++ src/main/java/ema/ui/game/settings/TwoPlayerSetting.java
@@ -1,5 +1,76 @@
 package ema.ui.game.settings;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
 public class TwoPlayerSetting {
-    
+    private JDialog dailog;
+
+    private JSlider points;
+
+    private JPanel buttonsPanel;
+
+    private JButton cancelBtn;
+
+    private JButton defaultBtn;
+
+    private JButton applyBtn;
+
+    public TwoPlayerSetting(JFrame homeFrame) {
+        this.dailog = new JDialog(homeFrame, "Two Player Settings", true);
+        this.points = new JSlider(1, 20);
+
+        this.buttonsPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        this.cancelBtn = new JButton("CANCEL");
+        this.defaultBtn = new JButton("DEFAULT");
+        this.applyBtn = new JButton("APPLY");
+
+        buttonsPanel.add(cancelBtn);
+        buttonsPanel.add(defaultBtn);
+        buttonsPanel.add(applyBtn);
+
+        dailog.setLocationRelativeTo(homeFrame);
+        dailog.setPreferredSize(new Dimension(400, 200));
+        dailog.setLayout(new BorderLayout());
+        
+        dailog.add(points, BorderLayout.CENTER);
+        dailog.add(buttonsPanel, BorderLayout.SOUTH);
+
+        cancelBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dailog.dispose();
+            }
+            
+        });
+
+        defaultBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                points.setValue(8);
+            }
+            
+        });
+
+        applyBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int pointsToWin = points.getValue();
+                System.out.println("Selected points: " + pointsToWin);
+                dailog.dispose();
+                // Open TwoPlayerFrame, setting values for the game
+            }
+            
+        });
+
+        dailog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        dailog.pack();
+    }
+
+    public JDialog getDialog() {
+        return this.dailog;
+    }
 }
