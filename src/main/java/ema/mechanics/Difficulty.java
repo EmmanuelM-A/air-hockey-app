def20975841417775cd553fb695560205e2bb123
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2024-08-20 19:03:44 +0100

    Changed the AI speed multiplier values.

--- src/main/java/ema/mechanics/Difficulty.java
+++ src/main/java/ema/mechanics/Difficulty.java
@@ -1,9 +1,9 @@
 package ema.mechanics;
 
 public enum Difficulty {
-    EASY(0.5),
-    NORMAL(0.9),
-    HARD(1.3);
+    EASY(0.7),
+    NORMAL(1.1),
+    HARD(1.7);
 
     private double speedMultiplier; 
 
