079b198e32186c15a4eb1d101f0c993214cf555c
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2024-08-22 08:36:54 +0100

    Adjusted the speed multiplier for the ai difficulty.

--- src/main/java/ema/mechanics/Difficulty.java
+++ src/main/java/ema/mechanics/Difficulty.java
@@ -1,9 +1,9 @@
 package ema.mechanics;
 
 public enum Difficulty {
-    EASY(0.9, 1.0),
-    NORMAL(1.5, 1.8),
-    HARD(2.0, 2.4);
+    EASY(0.75, 1.0),
+    NORMAL(1.0, 1.8),
+    HARD(1.5, 2.4);
 
     private double speedMultiplier; 
     private double diffMultiplier;
